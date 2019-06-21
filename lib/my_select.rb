3ad804683fc6d9897bb8d57f4ccf81eccc606cdf
Jordan Bacon <jmbacon92@gmail.com>
2019-06-21 07:55:18 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(collection)
   i = 0
   new_collection = []
   while i < collection.length
-    if yield(collection[i]) == true
+    if yield(collection[i]) == TRUE
+    new_collection << 
     i = i + 1
   end
     array
